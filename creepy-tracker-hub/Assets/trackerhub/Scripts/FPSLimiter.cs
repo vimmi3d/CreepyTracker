3ca0f74e1b4d42486ad045d6bb30ce6f1dfb89c9
Mauricio Sousa <antonio.sousa@ist.utl.pt>
2018-04-02 15:38:21 +0100

    FPSLimiter :+1:

---
+++ creepy-tracker-hub/Assets/trackerhub/Scripts/FPSLimiter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour {
+
+    public int fps = 24;
+
+	// Use this for initialization
+	void Start () {
+        Application.targetFrameRate = fps;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
